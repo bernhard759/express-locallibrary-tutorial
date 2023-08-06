05eaf5f0153657b63a801d8430e206e9cf696be6
bernhard759 <bernhard.gailer@gmx.de>
2023-08-06 14:00:15 +0200

    first commit

---
+++ models/author.js
@@ -0,0 +1,71 @@
+const { DateTime } = require("luxon");
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  family_name: { type: String, required: true, maxLength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date },
+});
+
+// Virtual for author's full name
+AuthorSchema.virtual("name").get(function () {
+  // To avoid errors in cases where an author does not have either a family name or first name
+  // We want to make sure we handle the exception by returning an empty string for that case
+  let fullname = "";
+  if (this.first_name && this.family_name) {
+    fullname = `${this.family_name}, ${this.first_name}`;
+  }
+
+  return fullname;
+});
+
+// Virtual for author's URL
+AuthorSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/catalog/author/${this._id}`;
+});
+
+// Author lifespan virtual property
+AuthorSchema.virtual("lifespan").get(function () {
+  let lifetime_string = "";
+  if (this.date_of_birth) {
+    lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+      DateTime.DATE_MED
+    );
+  } else {
+    return "no lifespan information";
+  }
+  lifetime_string += " - ";
+  if (this.date_of_death) {
+    lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocaleString(
+      DateTime.DATE_MED
+    );
+  } else {
+    lifetime_string += "today";
+  }
+  return lifetime_string;
+});
+
+// Virtual property for formatted date
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : "";
+});
+
+// Virtual property for formatted date
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+  return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : this.date_of_birth ? "today" : "";
+});
+
+AuthorSchema.virtual("date_of_birth_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_birth).toISODate(); // format 'YYYY-MM-DD'
+});
+
+AuthorSchema.virtual("date_of_death_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_death).toISODate(); // format 'YYYY-MM-DD'
+});
+
+// Export model
+module.exports = mongoose.model("Author", AuthorSchema);
